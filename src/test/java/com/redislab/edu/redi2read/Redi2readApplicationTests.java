c177e542e192cd20d34f7f19bb101692d1a07bc0
ESPINO CAMPOS,JUAN <jespinca@gruposantander.com>
2021-04-13 16:20:34 +0200

    first commit

---
+++ src/test/java/com/redislab/edu/redi2read/Redi2readApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redislab.edu.redi2read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redi2readApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

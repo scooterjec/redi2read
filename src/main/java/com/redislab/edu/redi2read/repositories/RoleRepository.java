c698eb875f93c9a7c2cc782e6238c7bb6273159a
ESPINO CAMPOS,JUAN <jespinca@gruposantander.com>
2021-04-14 16:05:38 +0200

    Training 3

---
+++ src/main/java/com/redislab/edu/redi2read/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.redislab.edu.redi2read.repositories;
+
+import com.redislab.edu.redi2read.models.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository  extends CrudRepository<Role, String>{
+    
+}

3f1ab3c657d859301c77ad406c2c47361f12b284
ESPINO CAMPOS,JUAN <jespinca@gruposantander.com>
2021-04-14 14:27:01 +0200

    Training 1 & 2

--- src/main/java/com/redislab/edu/redi2read/Redi2readApplication.java
+++ src/main/java/com/redislab/edu/redi2read/Redi2readApplication.java
@@ -2,9 +2,19 @@ package com.redislab.edu.redi2read;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class Redi2readApplication {
+
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<?, ?> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Redi2readApplication.class, args);

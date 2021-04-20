9cfa6a09a6c4fa26f5f25d199de06f50f48b0a5d
ESPINO CAMPOS,JUAN <jespinca@gruposantander.com>
2021-04-20 09:47:31 +0200

    Training 9

--- src/main/java/com/redislab/edu/redi2read/Redi2readApplication.java
+++ src/main/java/com/redislab/edu/redi2read/Redi2readApplication.java
@@ -1,13 +1,19 @@
 package com.redislab.edu.redi2read;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableCaching
 public class Redi2readApplication {
 
 	@Bean
@@ -20,6 +26,17 @@ public class Redi2readApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+			.prefixCacheNameWith(this.getClass().getPackageName() + ".")
+			.entryTtl(Duration.ofHours(1))
+			.disableCachingNullValues();
+		return RedisCacheManager.builder(connectionFactory)
+			.cacheDefaults(config)
+			.build();
 	}
 
 	public static void main(String[] args) {
